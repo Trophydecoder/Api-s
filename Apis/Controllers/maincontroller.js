5db6b946656713757282a96bdf5d4c278cf3e025
Trophydecoder <trophydecoder@gmail.com>
2024-11-07 10:36:27 +0200

    Create Refactoring 40%

--- Apis/Controllers/maincontroller.js
+++ Apis/Controllers/maincontroller.js
@@ -2,17 +2,49 @@ var Todos = require('../models/Todoslist');
 
 //CRUD OPERATIONS(CREATE(post),  READ(get),  UPDATE(put),  DELETE(delete))//
 
+//util functions//
+
+//check if list has data//
+function isEmptylist(obj) {
+  return !obj || obj.length == 0 || Object.keys(obj).length == 0;
+}
+
+//function to check if there ID we generated doesnt exist//
+function  existTodo(id){
+return Todos.find(Todo=> Todo.id==id)
+
+}
+// generateUniqueID function//
+function getUniqueID(Todos){
+     ///create random ID//
+     let min = 1000
+     let max = 9999
+     do{
+     var id = Math.floor(Math.random() * (max-min) + min)
+     }while (existTodo(id)) ;
+     return id;
+}
+
 module.exports.create = function (req,res){
-    ///create random ID//
-    let min = 1000
-    let max = 9999
-    let id = Math.floor(Math.random() * (max-min) + min)
+  //create a random iD//
+  if (isEmptylist(Todos)) {
+    providers = [];
+  }
+
+  var id = req.body.id;
+  if (existTodo(id)) {
+    res.status(400).send("Duplicate ID is not allowed");
+    id = getUniqueID(); //getNewID//
+  }
+
+  var Todo = req.body; //get new Todo
+  Todo.id = id;
 
 //new todo object
-let Todo = {
-    id :id,
-    title:req.body.title
-    }
+// let Todo = {
+//     id :id,
+//     title:req.body.title
+//     }
     //pushing new todo to TODOS//
     Todos.push(Todo)
     res.status(200).send(Todo)
