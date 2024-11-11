f6f81a0ebfaab8adb52cdc5dec86f8f5a528b278
Trophydecoder <trophydecoder@gmail.com>
2024-11-11 12:56:09 +0200

    deleteONe and DeleteMAny

--- Apis/Controllers/maincontroller.js
+++ Apis/Controllers/maincontroller.js
@@ -1,6 +1,6 @@
 var Todos = require("../models/Todoslist");
 const Todo = require("../mongodb/connnection");
-const {ObjectId} = require('mongodb')
+const { ObjectId } = require("mongodb");
 
 //CRUD OPERATIONS(CREATE(post),  READ(get),  UPDATE(put),  DELETE(delete))//
 
@@ -21,20 +21,18 @@ function handleError(res, error) {
 }
 
 module.exports.create = function (req, res) {
+  var todo = req.body; //get new Todo
 
-var todo = req.body; //get new Todo
-
-try {
-  Todo.create(todo)
-  .then(result => {
-    res.status(201).send(result)
-  })
-.catch((error) => handleError(res, error));
-} 
-catch (error) {
-  handleError(res, error);
-}
-}
+  try {
+    Todo.create(todo)
+      .then((result) => {
+        res.status(201).send(result);
+      })
+      .catch((error) => handleError(res, error));
+  } catch (error) {
+    handleError(res, error);
+  }
+};
 module.exports.readAll = function (req, res) {
   try {
     Todo.find()
@@ -51,59 +49,67 @@ module.exports.readAll = function (req, res) {
 };
 
 module.exports.readOne = function (req, res) {
-  try{
+  try {
     let id = new ObjectId(req.params.id);
-    Todo.find({'_id':id})
-      .then(result =>{
-          if (isEmptylist(result)) {
-              res.status(404).send("Cannot Read, Todo List is empty");
-            }
-          
-            res.status(200).send(result);
+    Todo.find({ _id: id })
+      .then((result) => {
+        if (isEmptylist(result)) {
+          res.status(404).send("Cannot Read, Todo List is empty");
+        }
+
+        res.status(200).send(result);
       })
       .catch((error) => handleError(res, error));
-  } 
-  catch (error) {
+  } catch (error) {
     handleError(res, error);
   }
-}
+};
 
 module.exports.update = function (req, res) {
- 
   try {
     let id = new ObjectId(req.params.id);
     let todo = req.body;
-    Todo.findOneAndUpdate({'_id':id},todo,{new:true})
-    .then (result => {
-      if (isEmptylist(result)) {
-        res.status(404).send("Todo List Is Empty,Cannot Update");
-      }
-       res.status(200).send(result);
-    })
-     .catch((error) => handleError(res, error));
+    Todo.findOneAndUpdate({ _id: id }, todo, { new: true })
+      .then((result) => {
+        if (isEmptylist(result)) {
+          res.status(404).send("Todo List Is Empty,Cannot Update");
+        }
+        res.status(200).send(result);
+      })
+      .catch((error) => handleError(res, error));
   } catch (error) {
     handleError(res, error);
   }
 };
 
 module.exports.deleteOne = function (req, res) {
-  if (isEmptylist(Todos)) {
-    res.status(404).send("Todo List Is Empty,Cannot Delete");
+  try {
+    let id = new ObjectId(req.params.id);
+    Todo.findOneAndDelete({ _id: id })
+      .then((result) => {
+        if (isEmptylist(result)) {
+          res.status(404).send("Todo List Is Empty,Cannot Delete");
+        }
+        res.status(200);
+        res.send(result);
+      })
+      .catch((error) => handleError(res, error));
+  } catch (error) {
+    handleError(res, error);
   }
-  let id = req.params.id;
-  let Todo = Todos.find((Todo) => Todo.id == id);
-  let index = Todos.indexOf(Todo);
-
-  //remove  the element at the index of idx
-  Todos.splice(index, 1);
-  res.status(200);
-  res.send(Todo);
 };
 
 module.exports.deleteAll = function (req, res) {
-  if (isEmptylist(Todos)) {
-    res.status(404).send("Todo List is Empty Cannot Delete");
+  try {
+    Todo.deleteMany({})
+      .then((result) => {
+        if (result.deletedCount === 0) {
+          res.status(404).send("Todo List is Empty Cannot Delete");
+        }
+        res.status(200).send("all Todos deleted ]n" + result);
+      })
+      .catch((error) => handleError(res, error));
+  } catch (error) {
+    handleError(res, error);
   }
-  Todos = [];
-  res.status(200).send("all Todos deleted");
 };
